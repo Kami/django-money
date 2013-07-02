611ba513b4c366dd452e0ca303032bea58737c6e
Tomaz Muraus <tomaz@tomaz.me>
2013-07-02 18:57:56 +0200

    Add new MoneyNoCurrencyInputWidget forum widget.

--- djmoney/forms/widgets.py
+++ djmoney/forms/widgets.py
@@ -3,7 +3,7 @@ from moneyed import Money, CURRENCIES, DEFAULT_CURRENCY_CODE
 from decimal import Decimal
 import operator
 
-__all__ = ('InputMoneyWidget', 'CurrencySelectWidget',)
+__all__ = ('InputMoneyWidget', 'CurrencySelectWidget', 'MoneyNoCurrencyInputWidget')
 
 CURRENCY_CHOICES = [(c.code, c.name) for i, c in CURRENCIES.items() if c.code != DEFAULT_CURRENCY_CODE]
 CURRENCY_CHOICES.sort(key=operator.itemgetter(1))
@@ -35,3 +35,31 @@ class InputMoneyWidget(forms.TextInput):
     
     def value_from_datadict(self, data, files, name):
         return (data.get(name, None), data.get(name+'_currency', None))
+
+
+class MoneyNoCurrencyInputWidget(InputMoneyWidget):
+    """
+    Custom Django Money widget which doesn't require user to enter a currency
+    and uses a default currency passed to the constructor.
+    """
+    def __init__(self, attrs=None, currency_widget=None,
+                 currency=DEFAULT_CURRENCY_CODE):
+        kwargs = {'attrs': attrs, 'currency_widget': currency_widget}
+        super(NoCurrencyMoneyInputWidget, self).__init__(**kwargs)
+        self.currency = currency
+
+    def render(self, name, value, attrs=None):
+        amount = ''
+        if isinstance(value, Money):
+            amount = value.amount
+        if isinstance(value, tuple):
+            amount = value[0]
+        if isinstance(value, int) or isinstance(value, Decimal):
+            amount = value
+        result = TextInput.render(self, name, amount)
+        return result
+
+    def value_from_datadict(self, data, files, name):
+        value = data.get(name, None)
+        currency = self.currency
+        return (value, currency)
